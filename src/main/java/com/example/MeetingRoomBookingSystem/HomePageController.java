ddbc497aad01392d5f7bdbbedfac8b003b34cf32
SandorDenis <denis.sandor00@e-uvt.ro>
2021-11-18 19:38:29 +0200

    Initial commit

---
+++ src/main/java/com/example/MeetingRoomBookingSystem/HomePageController.java
@@ -0,0 +1,12 @@
+package com.example.MeetingRoomBookingSystem;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomePageController {
+    @GetMapping("/index")
+    String home(){
+        return "index";
+    }
+}
---
+++ src/main/java/com/example/MeetingRoomBookingSystem/LoginPageController.java
@@ -0,0 +1,17 @@
+package com.example.MeetingRoomBookingSystem;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginPageController {
+    @GetMapping("/")
+    String signIn(){
+        return "login";
+    }
+
+    @GetMapping("/signup")
+    String signUp(){
+        return "signup";
+    }
+}
